2ded2787cc4b3829a7260de478cbe79a81cf3e80
Cindie Jouvin <cindie.jouvin@gmail.com>
2019-12-05 09:20:10 +0100

    update with current date

--- database/db.sql
+++ database/db.sql
@@ -18,14 +18,14 @@ CREATE TABLE admin
   latitude VARCHAR(4) NULL,
   longitude VARCHAR(4) NULL,
   phone_company VARCHAR(10) NOT NULL,
-  creation_date DATE NOT NULL
+  creation_date timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP
 );
 
 
-INSERT INTO TABLE admin 
+INSERT INTO admin 
 (company, firstname, lastname,login_admin,password_admin,city,zip,adress1,photo,phone_company,creation_date)
 VALUES
-('Camping de Max','Maxime','Thibaud','max-thi','password','Sainte Maxime','83120',"1 rue Thibaud",'https://zupimages.net/up/19/49/c3li.jpg','0401020304',2019-12-04),
-('Campind de Wojtek'),'Wojtek','Zdebski','woj-tek','password','Quiberon','56170','3 rue de la Steppe','https://zupimages.net/up/19/49/3ik8.jpg','0201020304',2019-12-04
-('Club de Victor','Victor','Biard','vic-bia','password','Reims','51100','6 rue du Champagne Comte Vicolatino','https://zupimages.net/up/19/49/y642.jpg','0301020304',2019-12-04),
-('Hotel de plein air de Cindie', 'Cindie','Jouvin','cin-jou','password','Cassis','Rue de la Framboise','https://zupimages.net/up/19/49/ctks.jpg','0406081001',2019-12-04);
+('Camping de Max','Maxime','Thibaud','max-thi','password','Sainte Maxime','83120','1 rue Thibaud','https://zupimages.net/up/19/49/c3li.jpg','0401020304','2019-12-04'),
+('Camping de Wojtek','Wojtek','Zdebski','woj-tek','password','Quiberon','56170','3 rue de la Steppe','https://zupimages.net/up/19/49/3ik8.jpg','0201020304','2019-12-04'),
+('Club de Victor','Victor','Biard','vic-bia','password','Reims','51100','6 rue du Champagne Comte Vicolatino','https://zupimages.net/up/19/49/y642.jpg','0301020304','2019-12-04'),
+('Hotel de plein air de Cindie', 'Cindie','Jouvin','cin-jou','password','Cassis','13260','Rue de la Framboise','https://zupimages.net/up/19/49/ctks.jpg','0406081001','2019-12-04');
