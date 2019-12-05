b29668bac38d62934869684a4c387650e7aba0c3
Cindie Jouvin <cindie.jouvin@gmail.com>
2019-12-05 17:50:08 +0100

    add css on component DisplayAdmin with new font

--- front/src/component/DisplayAdmin.js
+++ front/src/component/DisplayAdmin.js
@@ -4,13 +4,26 @@ import './DisplayAdmin.css';
 function DisplayAdmin(props){
   return(
   <div id="pagePrinc">
-     <h2 className="titreProfil">{props.camping.company}</h2>
-    <p>{props.camping.firstname} {props.camping.lastname}</p>
-    <p>{props.camping.address1}</p>
-    <p>{props.camping.zip} {props.camping.city}</p>
-    <p>{props.camping.phone_company}</p>
+    <div id="photoDeProfil">
+      <img className="photoProfil" src={props.camping.photo} alt="photo de profil de l'établissement touristique"></img>
+    </div>
+    <h2 className="titreProfil">{props.camping.company.toUpperCase()}</h2>
+    <div className="adressePres">
+      <p>{props.camping.address1} {props.camping.zip} {props.camping.city}</p>
+    </div>
+    <div id="phonePres">
+      <img className="logo" src='https://zupimages.net/up/19/49/m5nc.png' alt="logo tél"></img>
+      <p>{props.camping.phone_company}</p>
+    </div>
+    <div id="responsablePres">
+      <img className="logo" src='https://zupimages.net/up/19/49/xfg1.png' alt="logo resp"></img>
+      <p>Responsable : {props.camping.firstname} {props.camping.lastname}</p>
+    </div>
+    <div id="mailPres">
+      <img className="logo" src='https://zupimages.net/up/19/49/jijs.png' alt="logo email"></img>
+      <p>{props.camping.email_company}</p>
+    </div>
     <p>{props.camping.description_company}</p>
-    <img className="photoProfil" src={props.camping.photo} alt="photo de profil de l'établissement touristique"></img>
   </div>
   )
 }
