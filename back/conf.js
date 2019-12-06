2e98bdb1a1e6547a054b480e8ff25bd65edbda19
Maxime Thibaud <maximethibaudwcs@gmail.com>
2019-12-06 10:21:59 +0100

    a request GET one admin is working. conf.js is in the .gitignore for now

--- back/conf.js
+++ back/conf.js
@@ -2,7 +2,7 @@ const  mysql = require('mysql');
 const  connection = mysql.createConnection({
 host :  'localhost', // address of the server
 user :  'root', // username
-password :  'password',
+password :  'jecode4wcs',
 database :  'nytte_database',
 });
 module.exports = connection;
