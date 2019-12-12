1bfe86cc1c20bcca31abf8987587996f03ee9373
Maxime Thibaud <maximethibaudwcs@gmail.com>
2019-12-12 14:42:00 +0100

    banner added

--- front/src/component/FormPlace.js
+++ front/src/component/FormPlace.js
@@ -12,7 +12,12 @@ class FormPlace extends React.Component {
     render() {
       return (
       <div className = "formContainer">
-        <img src = 'https://zupimages.net/up/19/50/0b2g.png' className = 'LogoNytte' alt='logo Nytte'></img>
+        <div className="bannerTop">
+          <h1>Création
+            <br/>
+              d'un lieu
+          </h1>
+        </div>
         <form>
             <div className="form-example">
                 <label htmlFor="place">Nom du lieu</label>
@@ -24,9 +29,24 @@ class FormPlace extends React.Component {
                 <input type="tel" name="phone" id="phone" required/>
             </div>
             <hr/>
+            <div className="form-example">
+                <label htmlFor="photo">Photo du lieu</label>
+                <input type="text" name="photo" id="photo " required/>
+            </div>
+            <hr/>
+            <div className="form-example">
+                <label htmlFor="attachment">Pièce jointe</label>
+                <input type="text" name="attachment" id="attachment" required/>
+            </div>
+            <hr/>
+            <div className="form-example">
+                <label htmlFor="logo">Logo</label>
+                <input type="text" name="logo" id="logo" required/>
+            </div>
+            <hr/>
             <div className = 'textareaForm'>
                 <label htmlFor="description">
-                  <textarea className='textareaForm' placeholder='description du lieu' />
+                  <textarea className='textareaForm' placeholder='description du lieu' rows='5'/>
                 </label>
             </div>
             <hr/>
