1dfab2e7d4fa36864896194ab89943d0f449c81c
Wojtek <wojtek.zdebski@hotmail.fr>
2019-12-12 16:10:40 +0100

    apps calling modified - scroll admins

--- front/src/App.js
+++ front/src/App.js
@@ -2,38 +2,45 @@ import React from 'react';
 import axios from 'axios'
 import './App.css';
 import DisplayAdmin from './component/DisplayAdmin.js'
-
+import FormAdmin from './component/FormAdmin.js'
 
 class App extends React.Component {
-constructor(props) {
-  super(props);
-  this.state = { camping :{
-      id: 0,
-      company: '',
-      firstname: '',
-  }};
+  constructor(props) {
+    super(props);
+    this.state = {
+      campings : null,
+      currentCamping: 0
+    };
+    this.nextCamping = this.nextCamping.bind(this);
+  }
 
+  nextCamping(){
+    this.setState((prevState) => {
+      return {
+        currentCamping: (prevState.currentCamping + 1) % prevState.campings.length
+      };
+    });
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:8000/api/admins')
+    .then(response => response.data)
+    .then(data =>{
+      this.setState({
+        campings: data,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.campings && <DisplayAdmin camping={this.state.campings[this.state.currentCamping]}/>}
+        <button  type="button" onClick={this.nextCamping}>Suivant</button>
+        <FormAdmin />
+      </div>
+    );
+  }
 }
-
-componentDidMount() {
- 
-  axios.get('http://localhost:8000/api/admins')
-  .then(response => {
-    return response.data
-  })
-  .then(data =>{
-    this.setState({
-      camping: data[3]});
-  });
-}
-
-render() {
-  return (
-  <div>
-     <DisplayAdmin camping={this.state.camping}/>
-     <FormAdmin />
-  </div>
-);
-}}
 
 export default App;
