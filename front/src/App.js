f3af5bd5ae7e0cdbfeb7710c4e6a6e826df1f351
MaddMax51 <54673312+Maxime-Thibaud@users.noreply.github.com>
2019-12-13 09:57:37 +0100

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -43,7 +43,6 @@ class App extends React.Component {
         <FormAdmin />
       </div>
       <div>
-        <DisplayAdmin camping={this.state.camping}/>
         <FormPlace />
       </div>
     );
