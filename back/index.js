3315a0a923b2e648e70f356a93b9d8f5c893ca64
wilder <cindie.jouvin@gmail.com>
2019-12-17 14:35:57 +0100

    move of route on new file places.js

--- back/index.js
+++ back/index.js
@@ -10,29 +10,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
 const admins = require('./routes/admins');
-
+const places = require('./routes/places');
 app.use('/api/admins', admins);
+app.use('/api/places', places);
 
 
-app.get('/api/places', (request, response) => {
-  connection.query('SELECT local_name,local_photo,local_description,local_phone,local_pj,local_logo FROM place INNER JOIN admin WHERE place.admin_id=admin.id', [request.params.id], (err, results) => {
-   if (err) {
-     response.status(500).send('Error retrieving places');
-   } else {
-     response.json(results);
-   }
- });
-})
-
-app.get('/api/places/:id', (request, response) => {
-  connection.query('SELECT local_name,local_photo,local_description,local_phone,local_pj,local_logo FROM place INNER JOIN admin WHERE place.admin_id=admin.id AND place.id = ?', [request.params.id], (err, results) => {
-   if (err) {
-     response.status(500).send('Error retrieving places');
-   } else {
-     response.json(results);
-   }
- });
-})
 
 app.get('/api/vacationers', (request, response) => {
   connection.query('SELECT tourist_firstname,tourist_lastname,tourist_city,tourist_zip,tourist_address1,tourist_address2,tourist_photo,tourist_phone,tourist_email FROM vacationer INNER JOIN admin WHERE vacationer.admin_id=admin.id', [request.params.id], (err, results) => {
@@ -56,17 +38,7 @@ app.get('/api/vacationers/:id', (request, response) => {
 
 
 
-app.post('/api/places', (request, response) => {
-  const formData = request.body;
-  connection.query('INSERT INTO place SET ?', formData, (err, results) => {
-    if (err) {
-      console.log(err);
-      response.status(500).send("Error saving a new place");
-    } else {
-      response.sendStatus(200);
-    }
-  });
-});
+
 
 app.post('/api/vacationers', (request, response) => {
   const formData = request.body;
@@ -82,18 +54,7 @@ app.post('/api/vacationers', (request, response) => {
 
 
 
-app.put('/api/places/:id', (request, response) => {
-  const idPlace = request.params.id;
-  const formData = request.body;
-    connection.query('UPDATE place SET ? WHERE id = ?', [formData, idPlace], err => {
-    if (err) {
-      console.log(err);
-      response.status(500).send("Error editing the place");
-    } else {
-      response.sendStatus(200);
-    }
-  });
-});
+
 
 app.put('/api/vacationers/:id', (request, response) => {
   const idVacationer = request.params.id;
--- back/routes/places.js
+++ back/routes/places.js
@@ -2,50 +2,49 @@ const express = require('express');
 const router = express.Router();
 const connection = require("../conf");
 
-
 router.get('/', (request, response) => {
-    connection.query('SELECT * from admin', (err, results) => {
-     if (err) {
-       response.status(500).send('Error retrieving admins');
-     } else {
-       response.json(results);
-     }
-   });
-  })
-  
-  router.get('/:id', (request, response) => {
-    connection.query('SELECT * from admin where id = ?', [request.params.id], (err, results) => {
-     if (err) {
-       response.status(500).send('Error retrieving admins');
-     } else {
-       response.json(results);
-     }
-   });
-  })
+  connection.query('SELECT local_name,local_photo,local_description,local_phone,local_pj,local_logo FROM place INNER JOIN admin WHERE place.admin_id=admin.id', [request.params.id], (err, results) => {
+   if (err) {
+     response.status(500).send('Error retrieving places');
+   } else {
+     response.json(results);
+   }
+ });
+})
 
-  router.post('/api/admins', (request, response) => {
-    const formData = request.body;
-    connection.query('INSERT INTO admin SET ?', formData, (err, results) => {
-      if (err) {
-        console.log(err);
-        response.status(500).send("Error saving a new admin");
-      } else {
-        response.sendStatus(200);
-      }
-    });
-  });
+router.get('/:id', (request, response) => {
+  connection.query('SELECT local_name,local_photo,local_description,local_phone,local_pj,local_logo FROM place INNER JOIN admin WHERE place.admin_id=admin.id AND place.id = ?', [request.params.id], (err, results) => {
+   if (err) {
+     response.status(500).send('Error retrieving places');
+   } else {
+     response.json(results);
+   }
+ });
+})
 
-  router.put('/api/admins/:id', (request, response) => {
-    const idAdmin = request.params.id;
-    const formData = request.body;
-      connection.query('UPDATE admin SET ? WHERE id = ?', [formData, idAdmin], err => {
-      if (err) {
-        console.log(err);
-        response.status(500).send("Error editing the admin");
-      } else {
-        response.sendStatus(200);
-      }
-    });
+router.post('/', (request, response) => {
+  const formData = request.body;
+  connection.query('INSERT INTO place SET ?', formData, (err, results) => {
+    if (err) {
+      console.log(err);
+      response.status(500).send("Error saving a new place");
+    } else {
+      response.sendStatus(200);
+    }
   });
+});
+
+router.put('/:id', (request, response) => {
+  const idPlace = request.params.id;
+  const formData = request.body;
+    connection.query('UPDATE place SET ? WHERE id = ?', [formData, idPlace], err => {
+    if (err) {
+      console.log(err);
+      response.status(500).send("Error editing the place");
+    } else {
+      response.sendStatus(200);
+    }
+  });
+});
 
   module.exports = router
