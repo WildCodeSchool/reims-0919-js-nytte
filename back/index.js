e3acdc8ff58a988da5010563ad7371bd24da6745
wilder <cindie.jouvin@gmail.com>
2020-01-02 11:36:08 +0100

    add routes for debug

--- back/index.js
+++ back/index.js
@@ -6,9 +6,18 @@ const port = 8000;
 const connection = require("./conf");
 const jwt = require('jsonwebtoken');
 
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
+
+const admins = require('./routes/admins');
+const places = require('./routes/places');
+const vacationers = require('./routes/vacationers');
+app.use('/api/admins', admins);
+app.use('/api/places', places);
+app.use('/api/vacationers', vacationers);
+
 
 app.listen(port, (err) => {
   if (err) {
