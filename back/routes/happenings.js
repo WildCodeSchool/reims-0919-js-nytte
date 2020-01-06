8e558e8b0edc0b5a0421ea0c9d553a370ecfdb26
Wojtek <wojtek.zdebski@hotmail.fr>
2020-01-06 16:21:35 +0100

    created file routes and tests happenings

---
+++ back/routes/happenings.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+const connection = require("../conf");
+
+router.get('/', (request, response) => {
+  connection.query('SELECT happening_name, happening_picture, happening_category, happening_description, happening_date, happening_time, isItBookable FROM happening INNER JOIN place WHERE happening.place_id=place.id', [request.params.id], (err, results) => {
+   if (err) {
+    response.status(500).send('Error retrieving happening');
+   } else {
+    response.json(results);
+   }
+ });
+})
+
+
+router.get('/:id', (request, response) => {
+  connection.query('SELECT happening_name, happening_picture, happening_category, happening_description, happening_date, happening_time, isItBookable FROM happening INNER JOIN place WHERE happening.place_id=place.id AND happening.id = ?', [request.params.id], (err, results) => {
+   if (err) {
+    response.status(500).send('Error retrieving happening');
+   } else {
+    response.json(results);
+   }
+ });
+})
+
+router.post('/', (request, response) => {
+  const formData = request.body;
+  connection.query('INSERT INTO happening SET ?', formData, (err, results) => {
+    if (err) {
+      console.log(err);
+      response.status(500).send("Error saving a new happening");
+    } else {
+      response.sendStatus(200);
+    }
+  });
+});
+
+router.put('/:id', (request, response) => {
+  const idHappening = request.params.id;
+  const formData = request.body;
+    connection.query('UPDATE happening SET ? WHERE id = ?', [formData, idPlace], err => {
+    if (err) {
+      console.log(err);
+      response.status(500).send("Error editing the happening");
+    } else {
+      response.sendStatus(200);
+    }
+  });
+});
+
+  module.exports = router
