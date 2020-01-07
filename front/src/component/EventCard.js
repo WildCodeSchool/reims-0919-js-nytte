5af3638a6025ab0ef421a775ae0fe56c173e4177
wilder <cindie.jouvin@gmail.com>
2020-01-07 14:56:47 +0100

    add file Eventcard.js

---
+++ front/src/component/EventCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Display.css';
+
+function EventCard(props){
+  return(
+  <div id="pagePlace">
+    <div id="ProfilPlace">
+      <img className="photoProfil" src={props.place.local_photo} alt="profil du lieu touristique"></img>
+    </div>
+    <h2 className="titreProfil">{props.place.local_name}</h2>
+    <div className="desc">
+      <p>{props.place.local_description}</p>
+    </div>
+    <div id="phonePres">
+    {props.place.local_phone===null||<img className="logo" src='https://zupimages.net/up/19/49/m5nc.png' alt="logo tél"></img>}
+    {props.place.local_phone===null||<p>{props.place.local_phone}</p>}
+    </div>
+  </div>
+  )
+}
+
+export default EventCard;
