7d9972b771c9e9631f14ed515cd163f731081a99
wilder <cindie.jouvin@gmail.com>
2020-01-09 17:44:15 +0100

    modif on date and time

--- front/src/component/EventCardFull.js
+++ front/src/component/EventCardFull.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './EventCard.css';
 
-function EventCardFull(props){
+function EventCardFull({photo, category,description,date,time}){
   return(
   <div className="CardFull">
-    <div className="pictureCardLight">
-      <h1>{props.event.happening_category}</h1>
-      <img className="photoCardProfil" src={props.place.local_photo} alt="profil du lieu touristique"></img>
-      <button type="button" onClick=""></button>  
+    <div className="pictureCardFull">
+      <h1>{category.toUpperCase()}</h1>
+      <img className="photoCardFull" src={photo} alt="profil du lieu touristique"></img>
+    </div>
+    <div className="CardTextDescription">
+      <p>{description}</p>
+      <p>Date : {date.slice(0,10)} </p>
+      <p>Heure : {time.slice(0,5)}</p>
+      <p>Pour réserver, merci de vous rendre à l'accueil ou cliquer sur le bouton :</p>
+    </div>
+    <div className="fullCardButton">
+      <button className="BookButton" type="button" onClick="">RESERVER</button>
+      <button className="MapButton" type="button" onClick="">S'Y RENDRE</button>
     </div>
   </div>
   )
