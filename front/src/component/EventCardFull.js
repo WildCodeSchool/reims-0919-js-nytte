2688d8159d683c1190e1f5fb23586901e1d9293e
wilder <cindie.jouvin@gmail.com>
2020-01-14 19:15:06 +0100

    added list of category and linked with logo from a switch

--- front/src/component/EventCardFull.js
+++ front/src/component/EventCardFull.js
@@ -1,12 +1,37 @@
 import React from 'react';
 import './EventCard.css';
 
+function renderSwitch(category){
+  switch({category}){
+    case "Restauration":
+      return <img className="logoCategory" src="https://zupimages.net/up/20/03/o5vx.png" alt="logo de la catégorie"></img>;
+    case "Sport":
+      return <img className="logoCategory" src="https://zupimages.net/up/20/03/j9tf.png" alt="logo de la catégorie"></img>;
+    case "Enfant":
+      return <img className="logoCategory" src="https://zupimages.net/up/20/03/re66.png" alt="logo de la catégorie"></img>;
+    case "Fiesta":
+      return <img className="logoCategory" src="https://zupimages.net/up/20/03/o5vx.png" alt="logo de la catégorie"></img>;
+    case "Détente":
+      return <img className="logoCategory" src="https://zupimages.net/up/20/03/jjc3.png" alt="logo de la catégorie"></img>;
+    case "Nature":
+      return <img className="logoCategory" src="https://zupimages.net/up/20/03/5693.png" alt="logo de la catégorie"></img>;
+    case "Cours":
+      return <img className="logoCategory" src="https://zupimages.net/up/20/03/ay5x.png" alt="logo de la catégorie"></img>;
+    default:
+      console.log("yes")
+      return <img className="logoCategory" src="https://zupimages.net/up/20/03/zshx.png" alt="logo de la catégorie"></img>;  
+  }
+}
+
 function EventCardFull({photo, category,description,date,time}){
   return(
   <div className="CardFull">
     <div className="pictureCardFull">
-      <h1>{category.toUpperCase()}</h1>
-      <img className="photoCardFull" src={photo} alt="profil du lieu touristique"></img>
+      <div className="logoTitle">
+        {renderSwitch(category)}
+          <h1>{category.toUpperCase()}</h1>
+      </div>  
+        <img className="photoCardFull" src={photo} alt="profil du lieu touristique"></img>
     </div>
     <div className="CardTextDescription">
       <p>{description}</p>
@@ -16,7 +41,7 @@ function EventCardFull({photo, category,description,date,time}){
     </div>
     <div className="fullCardButton">
       <button className="BookButton" type="button">RESERVER</button>
-      <button className="MapButton" type="button">S'Y RENDRE</button>
+      <button className="MapButton" type="button">+ D'INFOS</button>
     </div>
   </div>
   )
