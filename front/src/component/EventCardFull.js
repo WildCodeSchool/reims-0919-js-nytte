9674274b2115cd0b7fd5f68e950362aa479ef647
wilder <cindie.jouvin@gmail.com>
2020-01-14 14:56:28 +0100

    implementation of ternary with time and date

--- front/src/component/EventCardFull.js
+++ front/src/component/EventCardFull.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './EventCard.css';
 
-function EventCardFull({photo, category,description,date,time}){
+function EventCardFull({photo, title, category,description,date,time,endTime, isItBookable}){
+  console.log(isItBookable)
   return(
   <div className="CardFull">
     <div className="pictureCardFull">
@@ -9,14 +10,28 @@ function EventCardFull({photo, category,description,date,time}){
       <img className="photoCardFull" src={photo} alt="profil du lieu touristique"></img>
     </div>
     <div className="CardTextDescription">
+      <h2>{title}</h2>
       <p>{description}</p>
-      <p>Date : {date.slice(0,10)} </p>
-      <p>Heure : {time.slice(0,5)}</p>
-      <p>Pour réserver, merci de vous rendre à l'accueil ou cliquer sur le bouton :</p>
-    </div>
-    <div className="fullCardButton">
-      <button className="BookButton" type="button">RESERVER</button>
-      <button className="MapButton" type="button">S'Y RENDRE</button>
+      {date===null
+        ?""
+        :<p>Date : {date.slice(0,10)} </p>}
+      {endTime===null
+        ?<p>à {time.slice(0,5)}</p>
+        :<p> Horaires : {time.slice(0,5)} - {endTime.slice(0,5)}</p>}
+      {isItBookable
+        ?<>
+        <p>Pour réserver, merci de vous rendre à l'accueil ou cliquer sur le bouton :</p>
+        <div className="fullCardButton">
+          <button className="BookButton" type="button">RESERVER</button>
+          <button className="MapButton" type="button">Y ALLER</button>
+        </div>
+        </>
+        :<div className="fullCardButton">
+          <button className="MapButton" type="button">Y ALLER</button>
+        </div>
+        }
+        
+        
     </div>
   </div>
   )
