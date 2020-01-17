21aa09d5594eff0deb58dc09507499a5555b3b77
wilder <cindie.jouvin@gmail.com>
2020-01-17 10:46:34 +0100

    commit before merge

--- back/routes/bookings.js
+++ back/routes/bookings.js
@@ -15,6 +15,15 @@ router.get('/', (request, response) => {
   });
 })
 
+router.get('/status', (request, response) => {
+    connection.query('select happening_id, seats_bookable,happening_name, happening_date, happening_time, COUNT(happening_id) AS places_booked, seats_bookable-COUNT(happening_id) AS free_places FROM booking INNER JOIN happening  WHERE happening_id=happening.id AND happening_date>=DATE(NOW()) AND seats_bookable IS NOT NULL GROUP BY happening_id ORDER BY happening_date ASC', (err, results) => {
+     if (err) {
+      response.status(500).send('Error retrieving booking');
+     } else {
+      response.json(results);
+     }
+   });
+})
 
 router.get('/:id', (request, response) => {
   connection.query("SELECT concat(happening_id,'-',tourist_id) AS num_book, happening_id, seats_bookable,	happening_name, happening_date, happening_time, tourist_id, tourist_lastname FROM booking INNER JOIN happening  ON happening_id=happening.id INNER JOIN vacationer ON tourist_id=vacationer.id WHERE happening.id=?", [request.params.id], (err, results) => {
@@ -26,15 +35,7 @@ router.get('/:id', (request, response) => {
   });
 })
 
-router.get('/status', (request, response) => {
-    connection.query('select happening_id, seats_bookable,happening_name, happening_date, happening_time, COUNT(happening_id) AS places_booked, seats_bookable-COUNT(happening_id) AS free_places FROM booking INNER JOIN happening  WHERE happening_id=happening.id AND happening_date>=DATE(NOW()) AND seats_bookable IS NOT NULL GROUP BY happening_id ORDER BY happening_date ASC', (err, results) => {
-     if (err) {
-      response.status(500).send('Error retrieving booking');
-     } else {
-      response.json(results);
-     }
-   });
-})
+
 
 router.post('/', (request, response) => {
     const formData = request.body;
