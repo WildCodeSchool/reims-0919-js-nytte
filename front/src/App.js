fa3594a19868d2101a05acd8c57cef6ae1cda1c4
Victor Biard <vi.biard@gmail.com>
2020-01-20 10:48:00 +0100

    Correction fail to compil cause an alone bracket in App.js

--- front/src/App.js
+++ front/src/App.js
@@ -89,7 +89,7 @@ class App extends React.Component {
           events: response.data
         })
       })
-    }
+    
   
     axios.get('http://localhost:8000/api/bookings/status')
       .then(response => response.data)
