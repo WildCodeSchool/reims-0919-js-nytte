2001c69533efd3b07ec172764de310c6eac51363
Wojtek <wojtek.zdebski@hotmail.fr>
2020-01-28 15:08:11 +0100

    fixedDventsFidUpdate

--- front/src/App.js
+++ front/src/App.js
@@ -155,7 +155,7 @@ class App extends React.Component {
       .then(axios.spread((places,happenings,vacationers,bookings)=>{
         this.setState({
           places: places.data,
-          happenings: happenings.data,
+          events: happenings.data,
           vacationers: vacationers.data,
           books: bookings.data,
           hasApiBeenCalledYet: true
